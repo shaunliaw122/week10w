7c62d51ad254d8cf81b35f25adbe8670a99e5569
shaunliaw122 <128031843+shaunliaw122@users.noreply.github.com>
2024-10-03 17:17:06 +1000

    test

---
+++ test.js
@@ -0,0 +1,10 @@
+var vg_1 = "map.vg.json";
+var vg_2 = "co2-emissions.vg.json"
+
+vegaEmbed("#map1", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#map2", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
